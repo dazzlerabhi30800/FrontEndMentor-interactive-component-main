ffdd89f1d668839350217b91afebd52deb047237
dazzlerabhi30800 <abhisingh30800@gmail.com>
2022-04-02 19:26:32 +0530

    Add alert container

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const rate = document.querySelector('.rate');
 const figureCard = document.querySelector('.card figure');
 const backCard = document.querySelector('.card .back');
 const frontCard = document.querySelector('.card .front');
+const alertContainer = document.querySelector('.alert__container');
 const tl = gsap.timeline({defaults : {duration : .75, ease:'power1.out'}});
 
 let score;
@@ -28,8 +29,17 @@ submitBtn.addEventListener('click', () => {
  
   if(score === undefined) {
     rate.innerText = "NoRating";
-    alert("Please select rating");
+    setTimeout(function () {
+      alertContainer.classList.add('show');
+    }, 100);
+    setTimeout(function () {
+      alertContainer.classList.remove('show')
+    }, 2000);
+    // alert("Please select rating");
   } else {
+    if(alertContainer.classList.contains('show')){
+      alertContainer.classList.remove('show');
+    }
     rate.innerText = score;
     frontCard.style.opacity = "0";
     backCard.style.opacity = "1";
@@ -38,4 +48,7 @@ submitBtn.addEventListener('click', () => {
     backCard.style.zIndex = "44";
   }
   console.log('it working');
-})
+});
+
+
+
